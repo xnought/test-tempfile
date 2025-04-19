c0e8e7208ff07f5e5626ac1a1f823e57207cd0f8
xnought <drbertucci@hotmail.com>
2025-04-19 01:46:53 -0400

    test

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def test_mkdtemp():
     shutil.rmtree(p)
 
 
+class TemporaryDirectoryPath(tempfile.TemporaryDirectory):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __enter__(self, *args, **kwargs):
+        return Path(super().__enter__(*args, **kwargs))  # gota love Path
+
+    def __exit__(self, *args, **kwargs):
+        super().__exit__(*args, **kwargs)
+
+
 def test_Temporary():
     with tempfile.TemporaryDirectory() as path_str:
         tempdir = Path(path_str)  # i prefer to use pathlib instead of str
@@ -31,10 +42,23 @@ def test_TempFile():
         print(d)
 
 
+def test_TemporaryPath():
+    with TemporaryDirectoryPath(dir=".cache", prefix="test") as tempdir:
+        with open(tempdir / "test.txt", "w") as f:
+            f.write("Hi there!")
+
+        print(os.listdir(tempdir))
+        while True:
+            continue
+
+    print("Dir was deleted?", not os.path.exists(tempdir))
+
+
 def main():
     # test_mkdtemp() # cons is that have to manually delete temporary dir
-    test_Temporary()
+    # test_Temporary()
     # test_TempFile()
+    test_TemporaryPath()
 
 
 if __name__ == "__main__":
